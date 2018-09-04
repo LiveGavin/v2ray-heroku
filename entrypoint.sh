aac83d372aa17d1b9d0e0e2bcf6c1b24e2d288cb
LiveGavin <26707207+LiveGavin@users.noreply.github.com>
2018-09-04 16:18:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
-cd /v2raybin
-wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
-unzip v2ray.zip 
-cd /v2raybin/v2ray-v$VER-linux-64
-chmod +x v2ray
+cd /vbin
+wget -O one.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
+unzip one.zip 
+mv /vbin/v2ray-v$VER-linux-64/* .
+
+mv v2ray one
+chmod +x one
 chmod +x v2ctl
 
 echo -e -n "$CONFIG_JSON1" > config.json
@@ -11,4 +13,4 @@ echo -e -n "$CONFIG_JSON2" >> config.json
 echo -e -n "$UUID" >> config.json
 echo -e -n "$CONFIG_JSON3" >> config.json
 
-./v2ray
+./one
